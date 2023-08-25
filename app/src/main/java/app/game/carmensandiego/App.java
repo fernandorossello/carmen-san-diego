e27da378b1b01cebb3166b1d132d6f913a558ad2
Fernando Rossello <fernando.rossello@glovoapp.com>
2023-08-25 18:57:02 +0200

    Make springboot application

--- app/src/main/java/app/game/carmensandiego/App.java
+++ app/src/main/java/app/game/carmensandiego/App.java
@@ -3,12 +3,13 @@
  */
 package app.game.carmensandiego;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        new SpringApplication(App.class).run(args);
     }
 }
